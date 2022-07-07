563d08329fdc35743835e7667d0882304e9af1a5
Никита Бурцев <nikbur97@yandex.ru>
2022-07-07 22:10:55 +0800

    bones

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello')
+from location import get_location
+from weather import get_weather
+from printer import print_weather
+
+
+def main():
+    location = get_location()
+    weather = get_weather(location)
+    print(print_weather(weather))
+
+
+if __name__ == '__main__':
+    main()
